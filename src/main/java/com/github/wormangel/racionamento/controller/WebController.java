6e3d950c253c17908b3531187ad4db730ba11d89
Lucas Medeiros de Azevedo <lucas.medeiros.de.azevedo@zalando.de>
2017-06-27 21:07:22 +0200

    Implement dynamic og-image for Facebook

--- src/main/java/com/github/wormangel/racionamento/controller/WebController.java
+++ src/main/java/com/github/wormangel/racionamento/controller/WebController.java
@@ -1,8 +1,19 @@
 package com.github.wormangel.racionamento.controller;
 
 import com.github.wormangel.racionamento.model.BoqueiraoStatistics;
+import com.github.wormangel.racionamento.service.ImageService;
 import com.github.wormangel.racionamento.service.StatisticsService;
+import com.github.wormangel.racionamento.service.spider.AesaSpider;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +26,27 @@ public class WebController {
     @Autowired
     private StatisticsService statisticsService;
 
+    @Autowired
+    private ImageService imageService;
+
     @RequestMapping(value = "/")
     public String home(Model model) throws IOException, ParseException {
         BoqueiraoStatistics stats = statisticsService.getStatistics();
         model.addAttribute("data", stats);
         return "index";
+    }
+
+    @RequestMapping(value = "/ogimage.png")
+    public ResponseEntity<Resource> ogImage() throws IOException, ParseException {
+        int days = statisticsService.getStatistics().getDaysToHappiness();
+
+        BufferedImage ogImage = imageService.getOgImage(days);
+
+        File destination = File.createTempFile("new", ".png");
+        ImageIO.write(ogImage, "png", destination);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(new InputStreamResource(new FileInputStream(destination)));
     }
 }
---
+++ src/main/java/com/github/wormangel/racionamento/service/ImageService.java
@@ -0,0 +1,66 @@
+package com.github.wormangel.racionamento.service;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+    @Cacheable("ogImage")
+    public BufferedImage getOgImage(int days) throws IOException {
+        // Generate the new image
+        Resource resource = new ClassPathResource("static/img/ogimg.png");
+        InputStream resourceInputStream = resource.getInputStream();
+        BufferedImage image = ImageIO.read(resourceInputStream);
+        Graphics graphics = image.getGraphics();
+
+        // Setup anti-aliasing
+        Graphics2D g2d = (Graphics2D) graphics;
+        g2d.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // What we want to write
+        String text = days + (days > 10 ? " dias" : " dia");
+
+        // Setup the font
+        graphics.setFont(new Font("Arial", Font.BOLD, 160));
+        Rectangle2D stringBounds = graphics.getFontMetrics().getStringBounds(text, graphics);
+
+        // Location to write - centered in the image
+        int x = (image.getWidth() - (int) stringBounds.getWidth()) / 2;
+        int y = ((image.getHeight() - (int) stringBounds.getHeight()) / 2) + (int) stringBounds.getHeight() - ((int) stringBounds.getHeight()) / 4;
+
+        // Draw the outline - hackish way!
+        graphics.setColor(Color.BLACK);
+        graphics.drawString(text, x - 1, y - 1);
+        graphics.drawString(text, x - 1, y + 1);
+        graphics.drawString(text, x + 1, y - 1);
+        graphics.drawString(text, x + 1, y + 1);
+        graphics.drawString(text, x - 2, y - 2);
+        graphics.drawString(text, x - 2, y + 2);
+        graphics.drawString(text, x + 2, y - 2);
+        graphics.drawString(text, x + 2, y + 2);
+        graphics.drawString(text, x - 3, y - 3);
+        graphics.drawString(text, x - 3, y + 3);
+        graphics.drawString(text, x + 3, y - 3);
+        graphics.drawString(text, x + 3, y + 3);
+
+        // Draw the text
+        graphics.setColor(Color.WHITE);
+        graphics.drawString(text, x, y);
+
+        return image;
+    }
+}
